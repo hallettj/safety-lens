1b4c3dbe9f5e6b39f0205fb3909cec4eace3536a
Jesse Hallett <jesse@sitr.us>
2015-07-01 19:33:56 -0700

    initial commit

---
+++ lens.js
@@ -0,0 +1,39 @@
+/* @flow */
+
+import type { IndexedCollection, List } from 'immutable'
+
+// export type Getter<S,A> = {
+//   to(obj: S): A
+// }
+
+// export type Setter<S,T,A,B> = {
+//   sets(f: (a: A) => B): (obj: S) => T
+// }
+
+// export type Lens<S,T,A,B> = Getter<S,A> & Setter<S,T,A,B>
+
+export type Lens<S,T,A,B> =
+  (f: (pure: Pure, val: A) => Functor<B>) => ((pure: Pure, obj: S) => Functor<T>)
+
+// export type Lens_<S,A> = Lens<S,S,A,A>
+
+export type Pure = <A>(val: A) => Functor<A>
+
+export type Functor<A> = {
+  map<B>(f: (val: A) => B): Functor<B>
+}
+
+function index<A,B>(idx: number): Lens<List<A>,List<B>,A,B> {
+  return f => (pure, list) => {
+    if (list.has(idx)) {
+      return f(pure, list.get(idx)).map(v => list.set(idx, v))
+    }
+    else {
+      return pure(list)
+    }
+  }
+}
+
+// type Lens' s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+
+// function compose(x: LensLike, y: LensLike): LensLike
