ac886bf560e14db94a6808dac423ca077e9a6c80
Jesse Hallett <jesse@sitr.us>
2015-07-02 01:52:37 -0700

    Implements `set`

--- lens.js
+++ lens.js
@@ -3,6 +3,7 @@
 export {
   compose,
   get,
+  set,
 }
 
 export type Lens<S,T,A,B> =
@@ -11,20 +12,34 @@ export type Lens<S,T,A,B> =
 export type Getting<R,S,A> =
   (f: (val: A) => Const<R,A>) => ((obj: S) => Const<R,S>)
 
+export type Setter<S,T,A,B> =
+  (f: (val: A) => Identity<B>) => ((obj: S) => Identity<T>)
+
 // export type Lens_<S,A> = Lens<S,S,A,A>
 
 export type Functor<A> = {
   map<B>(f: (val: A) => B): Functor<B>
 }
 
-type Const<A,B> = Functor<B> & { getConst: A }
+type Const<R,A> = Functor<A> & { getConst: R }
 
-function constant<A,B>(val: A): Const<A,B> {
+function constant<R,A>(val: R): Const<R,A> {
   var self = {
     getConst: val,
-    map: function<B>(_: any): Functor<B> { return self },
+    map<B>(f: (_: any) => B): Const<typeof val,B> { return self },
   }
   return self
+}
+
+type Identity<A> = Functor<A> & { getIdentity: A }
+
+function identity<A>(val: A): Identity<A> {
+  return {
+    getIdentity: val,
+    map<B>(f: (a: typeof val) => B): Identity<B> {
+      return identity(f(val))
+    },
+  }
 }
 
 // Ordinary function composition
@@ -32,6 +47,10 @@ function compose<A,B,C>(f: (_: B) => C, g: (_: A) => B): (_: A) => C {
   return x => f(g(x))
 }
 
-function get<S,A>(obj: S, getter: Getting<A,S,A>): A {
+function get<S,A>(getter: Getting<A,S,A>, obj: S): A {
   return getter(constant)(obj).getConst
+}
+
+function set<S,T,A,B>(setter: Setter<S,T,A,B>, val: B, obj: S): T {
+  return setter(_ => identity(val))(obj).getIdentity
 }
--- test/immutable.js
+++ test/immutable.js
@@ -1,23 +1,38 @@
 import { expect } from 'chai'
-import { List, is } from 'immutable'
-import { compose, get } from '../lens'
+import { List, fromJS, is } from 'immutable'
+import { compose, get, set } from '../lens'
 import { index } from '../immutable'
 
 describe('immutable', () => {
 
   var list = List([1,2,3,4])
-  var nestedList = List([List([1,2]), List([3,4])])
+  var nestedList = fromJS([[1, 2], [3, 4]])
 
   it('gets an index with `index`', () => {
     expect(
-      get(list, index(1))
+      get(index(1), list)
     )
     .to.equal(2)
   })
 
   it('gets nested indexes via composition', () => {
     var lens = compose(index(0), index(1))
-    expect(get(nestedList, lens)).to.equal(2)
+    expect(get(lens, nestedList)).to.equal(2)
+  })
+
+  it('sets an index with `index`', () => {
+    expect(
+      is( set(index(1), 5, list), List([1,5,3,4]) )
+    )
+    .to.be.true
+  })
+
+  it('sets nested indexes via composition', () => {
+    var lens = compose(index(0), index(1))
+    expect(
+      is( set(lens, 5, nestedList), fromJS([[1, 5], [3, 4]]) )
+    )
+    .to.be.true
   })
 
 })
