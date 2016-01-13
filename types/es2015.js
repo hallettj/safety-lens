adb5371c07ca73ee03c237c2b1316254f634e966
Jesse Hallett <jesse@sitr.us>
2016-01-13 02:19:54 -0800

    Remove public types/ dir; rename internal_types/ to types/
    We are now using .js.flow files instead of attempting to export type files.

--- types/es2015.js
+++
@@ -1,8 +0,0 @@
-import type { Lens, Lens_, Traversal_ } from 'safety-lens'
-
-declare module 'safety-lens/es2015' {
-  declare function index<A>(idx: number): Traversal_<A[],A>;
-  declare function prop<S:Object,A>(key: $Enum<S>): Lens_<S,A>;
-  declare function _1<A,B,C>(): Lens<[A,B],[C,B],A,C>;
-  declare function _2<A,B,C>(): Lens<[A,B],[A,C],B,C>;
-}
